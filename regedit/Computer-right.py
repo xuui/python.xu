547432595a3576354cbed221470168bc9f4a4e23
Xuhel <wangxu@ccc.net>
2019-12-16 11:18:40 +0800

    计算机右键菜单

---
+++ regedit/Computer-right.py
@@ -0,0 +1,11 @@
+﻿#!/usr/bin/python2.7
+# encoding:utf-8
+import shutil
+import winreg
+
+# Updata Registry.
+regKey=winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE,r"SOFTWARE\xuui\Classes\CLSID\{20D04FE0-3AEA-1069-A2D8-08002B30309D}\shell\DevMgr", 0, winreg.KEY_WRITE)
+#winreg.SetValueEx(regKey,"OEMBackground",0,winreg.REG_DWORD,1)
+winreg.SetValue(regKey,"@",0,winreg.REG_SZ,'设备(&Q)')
+winreg.SetValue(regKey,"icon",0,winreg.REG_SZ,'CompMgmtLauncher.exe')
+winreg.CloseKey(regKey)
