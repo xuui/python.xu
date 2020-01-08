fa3f675d3983f8e0aab650e1850b4b1b652303da
Xuhel <wangxu@ccc.net>
2020-01-08 13:35:15 +0800

    init Deadline

---
+++ Deadline/Deadline.py
@@ -0,0 +1,11 @@
+﻿#!/usr/bin/python2.7
+# encoding:utf-8
+# pip install GitPython
+
+import Deadline.DeadlineConnect as Connect
+connectionObject = Connect.DeadlineCon('WebServiceName', 8082)
+print connectionObject.Groups.GetGroupNames()
+#["group1","group2","group3"]
+#jobId = ...(valid job ID)
+#print connectionObject.Jobs.SuspendJob(jobId)
+
