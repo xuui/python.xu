b574cdd72b3c8ae6e9a612b494c2d6845a656a4d
Xuhel <wangxu@ccc.net>
2020-01-09 18:00:36 +0800

    Update gitRepo.py

--- python-git/gitRepo.py
+++ python-git/gitRepo.py
@@ -28,14 +28,16 @@ path='C:/cg-Package'
 #    repo.archive(fp)
 
 if os.path.exists(path):
-    repo = git.Repo.init(path)
     print(u"正在拉取更新中...")
+    repo = git.Repo.init(path)
     remote = repo.remote()
     remote.pull() # pull or push:remote.push()
     print(u"更新完毕.")
 else:
-    repo = git.Repo.clone_from(url='git@git.ccc.net:coco-code/package.git', to_path=path)
     print(u"正在获取更新中...")
+    repo = git.Repo.clone_from(url='git@git.ccc.net:coco-code/package.git', to_path=path)
     remote = repo.remote()
     remote.pull()
     print(u"更新完毕.")
+
+#git.Repo.init()
