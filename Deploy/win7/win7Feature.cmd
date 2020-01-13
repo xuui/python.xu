fd6b432efa657805503944b73626f756b5d1191d
Xuhel <wangxu@ccc.net>
2020-01-13 18:34:43 +0800

    Create win7Feature.cmd

---
+++ Deploy/win7/win7Feature.cmd
@@ -0,0 +1,17 @@
+rem dism /Image:wimount /Disable-Feature /FeatureName:
+dism /Image:I:\ /Disable-Feature /FeatureName:WindowsGadgetPlatform
+dism /Image:I:\ /Disable-Feature /FeatureName:"More Games"
+dism /Image:I:\ /Disable-Feature /FeatureName:Solitaire
+dism /Image:I:\ /Disable-Feature /FeatureName:SpiderSolitaire
+dism /Image:I:\ /Disable-Feature /FeatureName:Hearts
+dism /Image:I:\ /Disable-Feature /FeatureName:FreeCell
+dism /Image:I:\ /Disable-Feature /FeatureName:Minesweeper
+dism /Image:I:\ /Disable-Feature /FeatureName:PurblePlace
+dism /Image:I:\ /Disable-Feature /FeatureName:Chess
+dism /Image:I:\ /Disable-Feature /FeatureName:Shanghai
+dism /Image:I:\ /Disable-Feature /FeatureName:"Internet Games"
+dism /Image:I:\ /Disable-Feature /FeatureName:"Internet Checkers"
+dism /Image:I:\ /Disable-Feature /FeatureName:"Internet Backgammon"
+dism /Image:I:\ /Disable-Feature /FeatureName:"Internet Spades"
+dism /Image:I:\ /Disable-Feature /FeatureName:MediaCenter
+dism /Image:I:\ /Disable-Feature /FeatureName:OpticalMediaDisc
