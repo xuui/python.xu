35499a00484b8e7e9aa2bdbe122a4122ecbe10b2
Xuhel <wangxu@ccc.net>
2020-02-14 17:06:51 +0800

    Ubuntu AD

---
+++ Deploy/ubuntu/ubuntu-domain.sh
@@ -0,0 +1,19 @@
+# Ubuntu AD.
+
+echo /etc/hosts >> 127.0.0.1 aadds.contoso.com ubuntu
+
+sudo apt-get update
+sudo apt-get install krb5-user samba sssd sssd-tools libnss-sss libpam-sss ntp ntpdate realmd adcli
+
+sudo vi /etc/ntp.conf
+#
+#server aadds.contoso.com
+#
+
+sudo systemctl stop ntp
+sudo ntpdate aadds.contoso.com
+sudo systemctl start ntp
+
+sudo realm discover AADDS.CONTOSO.COM
+kinit contosoadmin@AADDS.CONTOSO.COM
+sudo realm join --verbose AADDS.CONTOSO.COM -U 'contosoadmin@AADDS.CONTOSO.COM' --install=/
