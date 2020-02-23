21f05502a8f4140f1688239397638c3fc2791772
Xuhel <xu.hel@outlook.com>
2020-02-23 22:04:12 +0800

    test psutil

---
+++ demo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3.8
+# encoding:utf-8
+# pip install GitPython
+
+# print('hello','World!')
+# name = input('please enter your name: ')
+# print('hello,', name)
+print('I\'m \"OK\"!')
+print('''line1
+... line2
+... line3''')
+print('''hello,\n
+world''')
+
+print('Hi, %s, you have $%d.' % ('Michael', 1000000))
