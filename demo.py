a7a60fc895f65692b7b4041c3e5206a1bd5d5bc2
xuhel <wangxu@ccc.net>
2020-06-19 10:52:36 +0800

    add pi py
    Signed-off-by: xuhel <wangxu@ccc.net>

--- demo.py
+++ demo.py
@@ -1,10 +1,15 @@
-#!/usr/bin/python3.8
+#!/usr/bin/python3
 # encoding:utf-8
 # pip install GitPython
 
 # print('hello','World!')
 # name = input('please enter your name: ')
 # print('hello,', name)
+
+import sys
+import os
+import getpass
+
 print('I\'m \"OK\"!')
 print('''line1
 ... line2
@@ -13,3 +18,13 @@ print('''hello,\n
 world''')
 
 print('Hi, %s, you have $%d.' % ('Michael', 1000000))
+
+#print os.getlogin()
+print getpass.getuser()
+#print getpass.getpass()
+
+content = os.popen('net user '+ getpass.getuser() +' /domain').read()
+print content
+
+
+print('37 ∷ ∴ ∵ × ÷ ° ℃ ‰')
